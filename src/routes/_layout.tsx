f49cd1a405655853768218e1589c2a3fc1f97355
Amiralis <sadeghi.amirali@live.com>
2025-07-29 11:44:28 +0200

    fix: layout minor height issue fixed

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -14,7 +14,7 @@ export const Route = createFileRoute('/_layout')({
 
 function LayoutComponent() {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex min-h-screen bg-gray-50">
       {/* Sidebar */}
       <div className="w-58 bg-white border-r border-gray-200 flex flex-col">
         {/* Logo Section */}
